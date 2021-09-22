b4df9550311c210072c87d3db2b77c63ac0a0cf7
Juandiacosta <acostasaenzjuandiego@gmail.com>
2021-09-21 21:20:14 -0500

    Console.log en acción
    Explorando el Console.log

---
+++ DOM/codigo.js
@@ -0,0 +1,11 @@
+var route = window.location;
+console.log(route.pathname);
+document.write("Estas en: " + route.pathname);
+
+var explora = navigator.appVersion; //Muestra la versión del navegador
+console.log(explora);
+document.write("Veamos que sorpresa encontramos " + explora);
+
+var documentando = document; //Muestra las partes del código en html
+console.log(documentando);
+document.write("Veamos " + documentando);
