a94d8d3645a633c6db2b6339a3336fae59660d71
Juandiacosta <acostasaenzjuandiego@gmail.com>
2021-09-25 21:03:55 -0500

    Fondos de Tablas
    Poniéndole el color de fondo a las tablas.

--- Canvas/diseno_tablas.js
+++ Canvas/diseno_tablas.js
@@ -3,42 +3,81 @@ var cantidadColumnas = document.getElementById("columnas");
 var azulMarino = document.getElementById("tablaAguaMarina");
 azulMarino.addEventListener("click", tablaMarina);
 var beige = document.getElementById("tablaBeige");
-beige.addEventListener("click", tablaBeigin);
+beige.addEventListener("click", tablaBeige);
 var blanco = document.getElementById("tablaBlanco");
 blanco.addEventListener("click", tablaBlanca);
 var oscuroAzul = document.getElementById("azulOscuro");
 oscuroAzul.addEventListener("click", lineasAzules);
-var oscuroNegro = document.getElementById("negro");
-oscuroNegro.addEventListener("click", lineasNegro);
-var intensoRojo = document.getElementById("rojo");
-intensoRojo.addEventListener("click", lineasRojas);
+// var oscuroNegro = document.getElementById("negro");
+// oscuroNegro.addEventListener("click", lineasNegro);
+// var intensoRojo = document.getElementById("rojo");
+// intensoRojo.addEventListener("click", lineasRojas);
 
 var estructura = document.getElementById("base");
 var ancho = estructura.width;
 var extenso = estructura.height;
-var lienzo = estructura.getContext('2d');
+var lienzo = estructura.getContext("2d");
 
-function dibujaFila(colorTrazo, xinicial, xfinal, yfinal)
+function dibujaFondo(colorFondo)
 {
     lienzo.beginPath();
-    // lienzo.fillStyle = colorFondo;
+    lienzo.strokeStyle = colorFondo;
+    lienzo.fillStyle = colorFondo;
+    lienzo.lineWidth = 2;
+    lienzo.moveTo(0, 0);
+    lienzo.lineTo(250, 0);
+    lienzo.lineTo(250, 100);
+    lienzo.lineTo(0, 100);
+    lienzo.lineTo(0, 0);
+    lienzo.stroke();
+    lienzo.fill();
+    lienzo.closePath();
+}
+
+function dibujaFila(colorTrazo, xinicial, yinicial, xfinal, yfinal)
+{
+    lienzo.beginPath();
     lienzo.strokeStyle = colorTrazo;
-    lienzo.lineWidt = 2;
+    lienzo.lineWidth = 2;
     lienzo.moveTo(xinicial, yinicial);
     lienzo.lineTo(xfinal, yfinal);
     lienzo.stroke();
     lienzo.closePath();
 }
 
-function dibujaFondo(colorFondo)
-{
-    lienzo.beginPath();
-    
-    lienzo.closePath();
-}
-
 function tablaMarina()
 {
     dibujaFondo("#03F5F7");
+}
+
+function tablaBeige()
+{
+    dibujaFondo("beige");
+}
+
+function tablaBlanca()
+{
+    dibujaFondo("whitesmoke");
+}
+
+function calculoLineasVerticales()
+{
+    var espacioVertical = ancho / lineas;
+    for(l = 0; l < lineas; l++)
+    {
+        yiv = ((l * espacioVertical) + 10);
+        yfv = (250 - ((l * espacioVertical)+ 10));
+        dibujaFila(colorTrazo, 0, yiv, 250, yfv);
+    }
+}
+
+function lineasAzules()
+{
+    var lineas = parseInt(cantidadFilas.value);
+    //var l = 0;
+    var yih, yfh, xiv, xfv;
+    var colorTrazo = "#5E01FF";
+
+    calculoLineasVerticales();
 }
 
